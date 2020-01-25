1e5f4aa66a78e167eb116bf4e3f971b53cdd10a0
Aaron Robinson <aaron@luncher.io>
2020-01-25 13:07:47 -0600

    Updated routes.

--- authentication/classroom-app/routes/index.js
+++ authentication/classroom-app/routes/index.js
@@ -2,15 +2,17 @@ const router = require('express').Router();
 const studentController = require('../controllers/student.controller');
 const authController = require('../controllers/auth.controller');
 
-router.get('/', authController.isLoggedIn, studentController.listStudents);
-router.get('/add', authController.isLoggedIn, studentController.addStudent);
-router.post('/update', authController.isLoggedIn, studentController.updateStudent);
-router.get('/delete/:id', authController.isLoggedIn, studentController.deleteStudent);
-router.get('/edit/:id', authController.isLoggedIn, studentController.editStudent);
 router.get('/register', authController.registerPage);
 router.post('/register', authController.registerUser);
 router.get('/login', authController.loginPage);
 router.post('/login', authController.loginUser);
 router.get('/logout', authController.logoutUser);
+
+router.use(authController.isLoggedIn); // Every route below requires authentication
+router.get('/', studentController.listStudents);
+router.get('/add', studentController.addStudent);
+router.post('/update', studentController.updateStudent);
+router.get('/delete/:id', studentController.deleteStudent);
+router.get('/edit/:id', studentController.editStudent);
 
 module.exports = router;
